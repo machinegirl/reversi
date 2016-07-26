bff649f21fa0baee922d53ac4a3862ff1cbca8f2
Jeremy Carter <Jeremy@JeremyCarter.ca>
2016-07-26 15:22:08 -0400

    static file serving and api route support fixed.

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,8 @@ extern crate staticfile;
 extern crate mount;
 extern crate router;
 
-// use iron::prelude::*;
 use iron::status;
-use iron::{Iron, Response, IronResult};
-use iron::request::Request;
+use iron::{Iron, Request, Response, IronResult};
 use staticfile::Static;
 use mount::Mount;
 use router::Router;
@@ -22,41 +20,39 @@ fn say_hello(req: &mut Request) -> IronResult<Response> {
 }
 
 fn main() {
-  let host_port = 8080; //Note: port must be 8080 for GAE
-  let hostname_cmd = Command::new("hostname").arg("-I").output();
-  let host_addr: SocketAddrV4 = match hostname_cmd {
-    Ok(res) => {
-      let addr = str::from_utf8(res.stdout.as_slice())
-          .map_err(|err| err.to_string())
-          .and_then(|ip_str| ip_str.trim()
-                      .parse::<Ipv4Addr>()
-                      .map_err(|err| err.to_string()))
-          .map(|ip| SocketAddrV4::new(ip, host_port));
+	let host_port = 8080; //Note: port must be 8080 for GAE
+	let hostname_cmd = Command::new("hostname").arg("-I").output();
+	let host_addr: SocketAddrV4 = match hostname_cmd {
+		Ok(res) => {
+			let addr = str::from_utf8(res.stdout.as_slice())
+				.map_err(|err| err.to_string())
+				.and_then(|ip_str| ip_str.trim()
+					.parse::<Ipv4Addr>()
+					.map_err(|err| err.to_string()))
+				.map(|ip| SocketAddrV4::new(ip, host_port));
+			match addr {
+				Ok(addr) => addr,
+				Err(_) => {
+					let ip = Ipv4Addr::new(127, 0, 0, 1);
+					SocketAddrV4::new(ip, host_port)
+				}
+			}
+		},
+		Err(_) => {
+			let ip = Ipv4Addr::new(127, 0, 0, 1);
+			SocketAddrV4::new(ip, host_port)
+		}
+	};
+	println!("Server listening at {}", host_addr);
 
-      match addr {
-        Ok(addr) => addr,
-        Err(_) => {
-          let ip = Ipv4Addr::new(127, 0, 0, 1);
-          SocketAddrV4::new(ip, host_port)
-        }
-      }
-    },
-    Err(_) => {
-      let ip = Ipv4Addr::new(127, 0, 0, 1);
-      SocketAddrV4::new(ip, host_port)
-    }
-  };
-  println!("Server listening at {}", host_addr);
+	// API routes
+	let mut router = Router::new();
+	router.get("/hello", say_hello);
 
-  let mut router = Router::new();
-  router.get("/hello", say_hello);
+	let mut mount = Mount::new();
+	mount
+		.mount("/api", router)
+		.mount("/", Static::new(Path::new("static/")));
 
-  let mut mount = Mount::new();
-
-  mount.mount("/", router);
-
-  Iron::new(mount).http(host_addr).unwrap();
-  // Iron::new(|_: &mut Request| {
-  //   Ok(Response::with((status::Ok, "Hello Rust Engine!")))
-  // }).http(host_addr).unwrap();
+	Iron::new(mount).http(host_addr).unwrap();
 }
