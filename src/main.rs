445d7591ab80285892f65dcabbb8c2044d90a22f
Jeremy Carter <Jeremy@JeremyCarter.ca>
2016-07-28 20:40:33 -0400

    Add green threads support (mioco) and a basic websocket server

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ extern crate iron;
 extern crate staticfile;
 extern crate mount;
 extern crate router;
+extern crate websocket;
+extern crate mioco;
 
 use iron::status;
 use iron::{Iron, Request, Response, IronResult};
@@ -13,6 +15,9 @@ use std::net::SocketAddrV4;
 use std::net::Ipv4Addr;
 use std::path::Path;
 use std::str;
+use websocket::{Server, Message, Sender, Receiver};
+use websocket::message::Type;
+use websocket::header::WebSocketProtocol;
 
 fn say_hello(req: &mut Request) -> IronResult<Response> {
 	println!("Running say_hello handler, URL path: {}", req.url.path().join("/"));
@@ -43,7 +48,7 @@ fn main() {
 			SocketAddrV4::new(ip, host_port)
 		}
 	};
-	println!("Server listening at {}", host_addr);
+	println!("Web server listening at http://{}", host_addr);
 
 	// API routes
 	let mut router = Router::new();
@@ -53,6 +58,62 @@ fn main() {
 	mount
 		.mount("/api", router)
 		.mount("/", Static::new(Path::new("static/dist/")));
+
+	mioco::start(|| {
+		mioco::spawn(move || {
+			let websocket_host = "127.0.0.1:8055";
+			let server = Server::bind(websocket_host).unwrap();
+			println!("WebSocket server listening at ws://{}", websocket_host);
+
+			for connection in server {
+				mioco::spawn(move || {
+					let request = connection.unwrap().read_request().unwrap(); // Get the request
+					let headers = request.headers.clone(); // Keep the headers so we can check them
+					request.validate().unwrap(); // Validate the request
+					let mut response = request.accept(); // Form a response
+
+					if let Some(&WebSocketProtocol(ref protocols)) = headers.get() {
+						if protocols.contains(&("rust-websocket".to_string())) {
+							// We have a protocol we want to use
+							response.headers.set(WebSocketProtocol(vec!["rust-websocket".to_string()]));
+						}
+					}
+
+					let mut client = response.send().unwrap(); // Send the response
+
+					let ip = client.get_mut_sender()
+						.get_mut()
+						.peer_addr()
+						.unwrap();
+
+					println!("Connection from {}", ip);
+
+					let message: Message = Message::text("Hello".to_string());
+					client.send_message(&message).unwrap();
+
+					let (mut sender, mut receiver) = client.split();
+
+					for message in receiver.incoming_messages() {
+						let message: Message = message.unwrap();
+
+						match message.opcode {
+							Type::Close => {
+								let message = Message::close();
+								sender.send_message(&message).unwrap();
+								println!("Client {} disconnected", ip);
+								return;
+							},
+							Type::Ping => {
+								let message = Message::pong(message.payload);
+								sender.send_message(&message).unwrap();
+							}
+							_ => sender.send_message(&message).unwrap(),
+						}
+					}
+				});
+			}
+		});
+	}).unwrap();
 
 	Iron::new(mount).http(host_addr).unwrap();
 }
