cb2a4dce78e929faf5e59324a7f1a36cc98bc81e
Jeremy Carter <Jeremy@JeremyCarter.ca>
2016-08-11 17:37:17 -0400

    Prepare views for google sign-in

--- static/src/app/main.ts
+++ static/src/app/main.ts
@@ -1,19 +1,18 @@
-import {WebsocketService} from './websocket.service';
 import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'App',
   template: require('./main.html'),
-  providers: [WebsocketService],
+  providers: [],
 })
 export class Main implements OnInit {
 
-  constructor(private websocketService: WebsocketService) {
+  constructor() {
 	//   reversiService.gameBoard = gameBoard;
 	  console.log('main controller started');
   }
 
   ngOnInit() {
-	  this.websocketService.init();
+
   }
 }
--- static/src/app/play.ts
+++ static/src/app/play.ts
@@ -1,19 +1,23 @@
-import {ReversiService} from './reversi.service';
 import { Component, OnInit} from '@angular/core';
+import {WebsocketService} from './websocket.service';
+import {ReversiService} from './reversi.service';
 
 @Component({
   selector: 'Play',
   template: require('./play.html'),
-  providers: [ReversiService],
+  providers: [WebsocketService, ReversiService],
 })
 export class Play implements OnInit {
 
-  constructor(private reversiService: ReversiService) {
+  constructor(private websocketService: WebsocketService, private reversiService: ReversiService) {
+	  this.websocketService = websocketService;
+	  this.reversiService = reversiService;
 	//   reversiService.gameBoard = gameBoard;
 	  console.log('play controller started');
   }
 
   ngOnInit() {
+	  this.websocketService.init();
 	  this.reversiService.init();
   }
 }
