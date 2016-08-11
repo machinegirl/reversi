1da582789ef7ecf0d83831d77f7708fc1138f8f1
Jeremy Carter <Jeremy@JeremyCarter.ca>
2016-08-11 16:05:25 -0400

    Fix wacky websocket client code

--- static/src/app/websocket.service.ts
+++ static/src/app/websocket.service.ts
@@ -5,204 +5,37 @@ import {ReversiService} from './reversi.service';
 export class WebsocketService {
 
   init() {
-	let sock;
-	let sock2;
-	let sock3;
 
-	let onopen = function(evt) {
-		if (sock && sock.readyState === 1) {
+	  let address = document.location.host.split(':')[0];
+	  let sockAddr;
 
-			if (sock2 && (sock2.readyState === 1)) {
-				sock2.onclose = undefined;
-				sock2.close();
-				sock2 = undefined;
-			}
+	  let tryConnecting = function() {
+		  if (address === 'localhost') {
+			  sockAddr = 'ws://127.0.0.1:8055';
+		  } else {
+			  sockAddr = 'ws://104.196.159.79:8055';
+		  }
 
-			if (sock3 && (sock3.readyState === 1)) {
-				sock3.onclose = undefined;
-				sock3.close();
-				sock3 = undefined;
-			}
+		  console.log('connecting to ' + sockAddr + ' ...');
 
-			sock.send(JSON.stringify({'cmd': 'msg', 'msg': 'client socket opened'}));
-			sock.send(JSON.stringify(evt));
-			ReversiService.startGame(sock);
-		}
+		  let sock = new WebSocket(sockAddr, 'rust-websocket');
 
-		if (sock2 && sock2.readyState === 1) {
+		  sock.onopen = function(evt) {
+	  		if (sock.readyState === 1) {
+				console.log('connected to ' + sockAddr);
+	  			sock.send(JSON.stringify({'cmd': 'msg', 'msg': 'client socket opened'}));
+	  			ReversiService.startGame(sock);
+	  		}
+	  	};
 
-			if (sock && (sock.readyState === 1)) {
-				sock.onclose = undefined;
-				sock.close();
-				sock = undefined;
-			}
+		sock.onclose = function() {
+			console.log('connection to ' + sockAddr + ' closed');
+			window.setTimeout(tryConnecting, 5000);
+		};
 
-			if (sock3 && (sock3.readyState === 1)) {
-				sock3.onclose = undefined;
-				sock3.close();
-				sock3 = undefined;
-			}
-
-			sock2.send(JSON.stringify({'cmd': 'msg', 'msg': 'client socket opened'}));
-			sock2.send(JSON.stringify(evt));
-			ReversiService.startGame(sock2);
-		}
-
-		if (sock3 && sock3.readyState === 1) {
-
-			if (sock && (sock.readyState === 1)) {
-				sock.onclose = undefined;
-				sock.close();
-				sock = undefined;
-			}
-
-			if (sock2 && (sock2.readyState === 1)) {
-				sock2.onclose = undefined;
-				sock2.close();
-				sock2 = undefined;
-			}
-
-			sock3.send(JSON.stringify({'cmd': 'msg', 'msg': 'client socket opened'}));
-			sock3.send(JSON.stringify(evt));
-			ReversiService.startGame(sock3);
-		}
 	};
 
-	let onmessage = function(evt) {
-		console.log('websocket msg');
-		console.log(evt);
-	};
+	window.setTimeout(tryConnecting, 5000);
 
-	let onerror = function(err) {
-		console.log('Error');
-		console.log(err);
-	};
-
-	let onclose1 = function() {
-		window.setTimeout(function() {
-			try {
-				window.setTimeout(function() {
-					sock = new WebSocket('ws://127.0.0.1:8055', 'rust-websocket');
-					if (sock) {
-						sock.onclose = onclose1;
-						sock.onopen = onopen;
-						sock.onmessage = onmessage;
-						sock.onerror = onerror;
-					}
-				}, 0);
-			} catch (err) {
-				console.log(err);
-				return;
-			}
-		}, 5000);
-	};
-
-	let onclose2 = function(evt) {
-		window.setTimeout(function() {
-			try {
-				window.setTimeout(function() {
-					sock2 = new WebSocket('ws://172.17.0.2:8055', 'rust-websocket');
-					if (sock2) {
-						sock2.onclose = onclose2;
-						sock2.onopen = onopen;
-						sock2.onmessage = onmessage;
-						sock2.onerror = onerror;
-					}
-				}, 0);
-			} catch (err) {
-				console.log(err);
-				return;
-			}
-		}, 5000);
-	};
-
-	let onclose3 = function(evt) {
-		window.setTimeout(function() {
-			try {
-				window.setTimeout(function() {
-					sock3 = new WebSocket('ws://104.196.159.79:8055', 'rust-websocket');
-					if (sock3) {
-						sock3.onclose = onclose3;
-						sock3.onopen = onopen;
-						sock3.onmessage = onmessage;
-						sock3.onerror = onerror;
-					}
-				}, 0);
-			} catch (err) {
-				console.log(err);
-				return;
-			}
-		}, 5000);
-	};
-
-	try {
-		window.setTimeout(function() {
-			sock = new WebSocket('ws://127.0.0.1:8055', 'rust-websocket');
-			if (sock) {
-				sock.onclose = onclose1;
-				sock.onopen = onopen;
-				sock.onmessage = onmessage;
-				sock.onerror = onerror;
-			}
-		}, 0);
-		window.setTimeout(function() {
-			sock2 = new WebSocket('ws://172.17.0.2:8055', 'rust-websocket');
-			if (sock2) {
-				sock2.onclose = onclose2;
-				sock2.onopen = onopen;
-				sock2.onmessage = onmessage;
-				sock2.onerror = onerror;
-			}
-		}, 0);
-		window.setTimeout(function() {
-
-			if (sock.readyState === 1 || sock2.readyState === 1) {
-				return;
-			}
-
-			sock3 = new WebSocket('ws://104.196.159.79:8055', 'rust-websocket');
-			if (sock3) {
-				sock3.onclose = onclose3;
-				sock3.onopen = onopen;
-				sock3.onmessage = onmessage;
-				sock3.onerror = onerror;
-			}
-
-		}, 0);
-	} catch (err) {
-		console.log(err);
-		return;
-	}
-
-	window.setInterval(function() {
-		if (sock && sock.readyState === 1) {
-			if (sock2 && sock2.readyState === 1) {
-				console.log('local websocket server detected, disconnecting from docker websocket server');
-				sock2.onclose = undefined;
-				sock2.close();
-				sock2 = undefined;
-			}
-			if (sock3 && sock3.readyState === 1) {
-				console.log('local websocket server detected, disconnecting from app engine websocket server');
-				sock3.onclose = undefined;
-				sock3.close();
-				sock3 = undefined;
-			}
-		}
-		if (sock2 && sock2.readyState === 1) {
-			if (sock && sock.readyState === 1) {
-				console.log('docker websocket server detected, disconnecting from local websocket server');
-				sock.onclose = undefined;
-				sock.close();
-				sock = undefined;
-			}
-			if (sock3 && sock3.readyState === 1) {
-				console.log('docker websocket server detected, disconnecting from app engine websocket server');
-				sock3.onclose = undefined;
-				sock3.close();
-				sock3 = undefined;
-			}
-		}
-	}, 5000);
   }
 }
