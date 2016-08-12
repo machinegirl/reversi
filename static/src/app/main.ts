e7ba3d589a23572d95fd1d4d3f6e23dc7a20c50f
Daphne <machinegirl@eternalvoid.net>
2016-08-12 15:17:03 -0400

    adds google login support

--- static/src/app/main.ts
+++ static/src/app/main.ts
@@ -1,24 +1,49 @@
 import { Component, OnInit, Input} from '@angular/core';
+import {WebsocketService} from './websocket.service';
 
 @Component({
   selector: 'App',
   template: require('./main.html'),
-  providers: [],
+  providers: [WebsocketService],
 })
 export class Main implements OnInit {
 
   @Input() public main: Main;
 
-  constructor() {
+  // public websocketService: WebsocketService;
+
+  constructor(private websocketService: WebsocketService) {
 	//   reversiService.gameBoard = gameBoard;
+	this.websocketService = websocketService;
 	  console.log('main controller started');
+
   }
 
 	ngOnInit() {
-		(<any>window).onSignIn = function(googleUser) {
+		this.websocketService.init();
+		(<any>window).onSignIn = (function(googleUser) {
 			let profile = googleUser.getBasicProfile();
 			console.log('Name: ' + profile.getName());
+			console.log('Token: ' + googleUser.getAuthResponse().id_token);
+
 			// TODO: send googleUser.getAuthResponse().id_token to backend over websocket
-		  };
+
+			let sendMsgIntHandle = 	window.setInterval((function() {
+				if (typeof this.websocketService !== 'undefined' && typeof this.websocketService.sock !== 'undefined') {
+					this.websocketService.sock.send(JSON.stringify({
+						'cmd': 'login',
+						'id_token': googleUser.getAuthResponse().id_token
+					}));
+
+					window.clearInterval(sendMsgIntHandle);
+				} else {
+					console.log('trying again...')
+				}
+			}).bind(this), 5000);
+
+		}).bind(this);
+
+
 	}
+
 }
--- static/src/app/websocket.service.ts
+++ static/src/app/websocket.service.ts
@@ -4,12 +4,14 @@ import {ReversiService} from './reversi.service';
 @Injectable()
 export class WebsocketService {
 
+  public sock: any;
+
   init() {
 
 	  let address = document.location.host.split(':')[0];
 	  let sockAddr;
 
-	  let tryConnecting = function() {
+	  let tryConnecting = (function() {
 		  if (address === 'localhost') {
 			  sockAddr = 'ws://127.0.0.1:8055';
 		  } else {
@@ -18,24 +20,24 @@ export class WebsocketService {
 
 		  console.log('connecting to ' + sockAddr + ' ...');
 
-		  let sock = new WebSocket(sockAddr, 'rust-websocket');
+		  this.sock = new WebSocket(sockAddr, 'rust-websocket');
 
-		  sock.onopen = function(evt) {
-	  		if (sock.readyState === 1) {
+		  this.sock.onopen = (function(evt) {
+	  		if (this.sock.readyState === 1) {
 				console.log('connected to ' + sockAddr);
-	  			sock.send(JSON.stringify({'cmd': 'msg', 'msg': 'client socket opened'}));
+	  			this.sock.send(JSON.stringify({'cmd': 'msg', 'msg': 'client socket opened'}));
 				// 		ReversiService.startGame(sock);
 	  		}
-	  	};
+	  	}).bind(this);
 
-		sock.onclose = function() {
+		this.sock.onclose = (function() {
 			console.log('connection to ' + sockAddr + ' closed');
-			window.setTimeout(tryConnecting, 5000);
-		};
+			window.setTimeout(tryConnecting.bind(this), 5000);
+		}).bind(this);
 
-	};
+	}).bind(this);
 
-	window.setTimeout(tryConnecting, 5000);
+	window.setTimeout(tryConnecting.bind(this), 5000);
 
   }
 }
