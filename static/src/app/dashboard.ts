abd40ef1cfe0a22780207df92e40533e2bf70537
Daphne <machinegirl@eternalvoid.net>
2016-08-15 18:04:49 -0400

    sends logout message to backend

--- static/src/app/dashboard.ts
+++ static/src/app/dashboard.ts
@@ -53,14 +53,26 @@ export class Dashboard implements OnInit {
 		(<any>window).gapi.load('client:auth2', this.authInit.bind(this));
 		// localStorage.removeItem('google_id_token');
 		// let resp: any;
-		let idToken = localStorage.getItem('google_id_token');
-		this.http.get('https://accounts.google.com/o/oauth2/revoke?token={' + idToken + '}')
-			.subscribe(
-				data => console.log(data),
-				err => console.log(err),
-				() => console.log('done')
-			);
-		// console.log(resp);
+		let sendMsgIntHandle =  window.setInterval((function() {
+			let idToken = localStorage.getItem('google_id_token');
+			if (typeof this.websocketService !== 'undefined' && typeof this.websocketService.sock !== 'undefined' && this.websocketService.sock.readyState === 1) {
+				this.websocketService.sock.send(JSON.stringify({
+					'cmd': 'logout',
+					'id_token': idToken
+				}));
+
+				window.clearInterval(sendMsgIntHandle);
+		   } else {
+			   //do nothing
+		   }
+	   }).bind(this), 500);
+
+		// this.http.get('https://accounts.google.com/o/oauth2/revoke?token=' + idToken)
+		// 	.subscribe(
+		// 		data => console.log(data),
+		// 		err => console.log(err),
+		// 		() => console.log('done')
+		// 	);
 	}
 
 	authInit() {
@@ -73,7 +85,7 @@ export class Dashboard implements OnInit {
 
 	authCallback(googleAuth) {
 		googleAuth.signOut();
-		window.location.assign('/');
+		// window.location.assign('/');
 	}
 
 }
