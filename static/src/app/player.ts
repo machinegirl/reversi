9eafdbf7fad7170a53ef47d41ec9e8ebf157c13a
Daphne <machinegirl@eternalvoid.net>
2016-08-17 19:13:43 -0400

    bug fix

--- static/src/app/player.ts
+++ static/src/app/player.ts
@@ -16,6 +16,9 @@ export class Player implements OnInit {
 
 	ngOnInit() {
 		this.path = window.location.pathname;
+		if (this.path === '/play') {
+			return;
+		}
 		let sendMsgIntHandle =  window.setInterval((function() {
 			let idToken = localStorage.getItem('google_id_token');
 			if (typeof this.websocketService !== 'undefined' && typeof this.websocketService.sock !== 'undefined' && this.websocketService.sock.readyState === 1) {
